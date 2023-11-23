25c891d57ae94a0411f8ef58d621cdba7e93a469
tricepad <tricepa.dornon@gmail.com>
2023-11-23 22:35:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var timerElement = document.getElementById('timer');
-    var endSound = document.getElementById('endSound');
+    var timerElement = document.getElementById('timer-label');
     var customTimeInput = document.getElementById('customTime');
     var startButton = document.getElementById('startButton');
     var stopButton = document.getElementById('stopButton');
     var timeButtons = document.querySelectorAll('.timeButton');
+    var endSound = document.getElementById('endSound');
+    var timerPathRemaining = document.getElementById('timer-path-remaining');
     var interval;
-    var lastTimeSelected = 30; // Conserver la dernière valeur sélectionnée
-    var timeLeft = lastTimeSelected; // Initialiser timeLeft avec lastTimeSelected
+    var totalTime = 30; // Temps total en secondes
+    var timeLeft = totalTime;
+
+    // Mettre à jour le cercle de progression SVG
+    function setCircleDasharray() {
+        const circleDasharray = `${(timeLeft / totalTime) * 283} 283`;
+        timerPathRemaining.setAttribute('stroke-dasharray', circleDasharray);
+    }
 
     function updateTimerDisplay(newTime) {
-        lastTimeSelected = newTime; // Mettre à jour lastTimeSelected avec le nouveau temps
-        timeLeft = newTime; // Mettre à jour timeLeft avec le nouveau temps
-        timerElement.textContent = timeLeft; // Afficher le nouveau temps
+        totalTime = newTime;
+        timeLeft = newTime;
+        timerElement.textContent = timeLeft.toString().padStart(2, '0');
+        setCircleDasharray(); // Mettre à jour le cercle de progression
     }
 
     function startTimer() {
         clearInterval(interval);
         interval = setInterval(function() {
+            timeLeft--;
+            timerElement.textContent = timeLeft.toString().padStart(2, '0');
+            setCircleDasharray(); // Mettre à jour le cercle de progression
+
             if (timeLeft <= 0) {
                 clearInterval(interval);
                 endSound.play();
-            } else {
-                timerElement.textContent = timeLeft;
-                timeLeft--;
+                timeLeft = totalTime; // Réinitialiser le temps restant
+                setCircleDasharray(); // Réinitialiser le cercle de progression
             }
         }, 1000);
     }
 
+    // Arrêter le timer et réinitialiser le cercle de progression
     function stopTimer() {
         clearInterval(interval);
-        updateTimerDisplay(lastTimeSelected); // Réinitialiser le timer à lastTimeSelected
+        updateTimerDisplay(totalTime);
     }
 
-    // Les boutons de temps prédéfini devraient également démarrer le timer après la mise à jour de l'affichage.
-timeButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        updateTimerDisplay(parseInt(this.dataset.time, 10));
-        startTimer(); // Ajoutez cet appel pour démarrer le timer avec le temps prédéfini
-    });
-});
-
-    startButton.addEventListener('click', function() {
-    var customTime = parseInt(customTimeInput.value, 10);
-    if (!isNaN(customTime) && customTime > 0) {
-        updateTimerDisplay(customTime);
-        startTimer(); // Démarrez le timer avec la valeur personnalisée
-    }
-});
-
+    // Ajout des écouteurs d'événements pour les boutons
+    startButton.addEventListener('click', startTimer);
     stopButton.addEventListener('click', stopTimer);
 
+    timeButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            var newTime = parseInt(this.dataset.time, 10);
+            updateTimerDisplay(newTime);
+        });
+    });
+
     endSound.addEventListener('ended', function() {
-        updateTimerDisplay(30); // Réinitialiser le timer après la fin du son
+        stopTimer(); // Arrêter le timer et réinitialiser le cercle de progression
     });
 });
