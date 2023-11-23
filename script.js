2b474a6ca84bc79f44e4bf015be566318829ce02
tricepad <tricepa.dornon@gmail.com>
2023-11-23 18:29:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,41 +6,41 @@ document.addEventListener('DOMContentLoaded', function() {
     var stopButton = document.getElementById('stopButton');
     var timeButtons = document.querySelectorAll('.timeButton');
     var interval;
-    var timeLeft = 30; // Durée initiale du timer
+    var timeLeft = 30; // Déclaration globale de timeLeft
 
     function updateTimerDisplay(newTime) {
-        timeLeft = newTime; // Mettre à jour le temps restant
-        timerElement.textContent = timeLeft; // Mettre à jour l'affichage
+        timeLeft = newTime;
+        timerElement.textContent = timeLeft;
     }
 
     function startTimer() {
-        clearInterval(interval); // Arrêter tout timer existant
+        clearInterval(interval);
         interval = setInterval(function() {
             if (timeLeft <= 0) {
-                clearInterval(interval); // Arrêter le timer si le temps est écoulé
-                endSound.play(); // Jouer le son
+                clearInterval(interval);
+                endSound.play();
             } else {
-                timerElement.textContent = timeLeft; // Afficher le temps restant
-                timeLeft--; // Décrémenter le temps restant
+                timerElement.textContent = timeLeft;
+                timeLeft--; // Assurez-vous que timeLeft est accessible ici
             }
-        }, 1000); // Répéter toutes les secondes
+        }, 1000);
     }
 
     function stopTimer() {
-        clearInterval(interval); // Arrêter le timer
-        updateTimerDisplay(timeLeft); // Afficher le temps restant
+        clearInterval(interval);
+        updateTimerDisplay(timeLeft); // timeLeft doit être accessible ici
     }
 
     timeButtons.forEach(function(button) {
         button.addEventListener('click', function() {
-            updateTimerDisplay(parseInt(this.dataset.time, 10)); // Mettre à jour le timer avec le temps du bouton cliqué
+            updateTimerDisplay(parseInt(this.dataset.time, 10));
         });
     });
 
-    startButton.addEventListener('click', startTimer); // Démarrer le timer lorsque le bouton Démarrer est cliqué
-    stopButton.addEventListener('click', stopTimer); // Arrêter le timer lorsque le bouton Stop est cliqué
+    startButton.addEventListener('click', startTimer);
+    stopButton.addEventListener('click', stopTimer);
 
     endSound.addEventListener('ended', function() {
-        updateTimerDisplay(30); // Réinitialiser à 30 secondes après la fin du son
+        updateTimerDisplay(30);
     });
 });
