2f2c4adfd979d4e460e82eb61de934f4f0a06c42
tricepad <tricepa.dornon@gmail.com>
2023-11-23 18:32:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("Le DOM est chargé"); // Confirmer que le DOM est chargé
     var timerElement = document.getElementById('timer');
     var endSound = document.getElementById('endSound');
     var customTimeInput = document.getElementById('customTime');
@@ -6,29 +7,33 @@ document.addEventListener('DOMContentLoaded', function() {
     var stopButton = document.getElementById('stopButton');
     var timeButtons = document.querySelectorAll('.timeButton');
     var interval;
-    var timeLeft = 30; // Déclaration globale de timeLeft
+    var timeLeft = 30;
 
     function updateTimerDisplay(newTime) {
+        console.log("Mise à jour de l'affichage du timer avec", newTime);
         timeLeft = newTime;
         timerElement.textContent = timeLeft;
     }
 
     function startTimer() {
+        console.log("Démarrage du timer");
         clearInterval(interval);
         interval = setInterval(function() {
+            console.log("Timer en cours:", timeLeft);
             if (timeLeft <= 0) {
                 clearInterval(interval);
                 endSound.play();
             } else {
                 timerElement.textContent = timeLeft;
-                timeLeft--; // Assurez-vous que timeLeft est accessible ici
+                timeLeft--;
             }
         }, 1000);
     }
 
     function stopTimer() {
+        console.log("Arrêt du timer");
         clearInterval(interval);
-        updateTimerDisplay(timeLeft); // timeLeft doit être accessible ici
+        updateTimerDisplay(timeLeft);
     }
 
     timeButtons.forEach(function(button) {
