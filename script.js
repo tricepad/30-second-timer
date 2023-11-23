0a6bbfb862c0aded606a5dea635c3db769ce660a
tricepad <tricepa.dornon@gmail.com>
2023-11-23 16:52:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 document.getElementById('startButton').addEventListener('click', function() {
-    var initialTime = 30; // Valeur initiale du timer
-    var timeLeft = initialTime;
+    var initialTime = 30;
     var timerElement = document.getElementById('timer');
-    var interval = setInterval(function() {
-        if (timeLeft <= 0) {
-            clearInterval(interval);
-            document.getElementById('endSound').play();
-            timeLeft = initialTime; // Réinitialiser le temps
-            timerElement.textContent = initialTime; // Mettre à jour l'affichage
-            // Optionnel : Redémarrer automatiquement ou permettre à l'utilisateur de redémarrer manuellement
-        } else {
-            timerElement.textContent = timeLeft;
-            timeLeft -= 1;
-        }
-    }, 1000);
+    var endSound = document.getElementById('endSound');
+
+    // Réinitialiser le timer à sa valeur initiale et mettre à jour l'affichage.
+    function resetTimer() {
+        timerElement.textContent = initialTime;
+    }
+
+    // Fonction pour démarrer le timer.
+    function startTimer() {
+        var timeLeft = initialTime;
+        var interval = setInterval(function() {
+            if (timeLeft <= 0) {
+                clearInterval(interval);
+                endSound.play();
+            } else {
+                timerElement.textContent = timeLeft;
+                timeLeft -= 1;
+            }
+        }, 1000);
+    }
+
+    // Écouter l'événement 'ended' sur l'élément audio pour réinitialiser le timer
+    // après que le son est terminé.
+    endSound.addEventListener('ended', resetTimer);
+
+    // Démarrer le timer.
+    startTimer();
 });
