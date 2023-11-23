b180286c25414eb9870311e744e2999db3ce03f2
tricepad <tricepa.dornon@gmail.com>
2023-11-23 16:58:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-document.getElementById('startButton').addEventListener('click', function() {
-    var initialTime = 30;
+document.addEventListener('DOMContentLoaded', function() {
     var timerElement = document.getElementById('timer');
     var endSound = document.getElementById('endSound');
+    var customTimeInput = document.getElementById('customTime');
+    var startButton = document.getElementById('startButton');
+    var timeButtons = document.querySelectorAll('.timeButton');
+    var interval;
+    var timeLeft;
 
-    // Réinitialiser le timer à sa valeur initiale et mettre à jour l'affichage.
-    function resetTimer() {
-        timerElement.textContent = initialTime;
+    // Fonction pour mettre à jour le timer.
+    function updateTimer(newTime) {
+        timeLeft = newTime;
+        timerElement.textContent = timeLeft;
     }
 
     // Fonction pour démarrer le timer.
     function startTimer() {
-        var timeLeft = initialTime;
-        var interval = setInterval(function() {
+        if (interval) {
+            clearInterval(interval); // Assurez-vous de nettoyer l'interval existant.
+        }
+        interval = setInterval(function() {
             if (timeLeft <= 0) {
                 clearInterval(interval);
                 endSound.play();
@@ -22,10 +29,27 @@ document.getElementById('startButton').addEventListener('click', function() {
         }, 1000);
     }
 
-    // Écouter l'événement 'ended' sur l'élément audio pour réinitialiser le timer
-    // après que le son est terminé.
+    // Réinitialiser le timer à sa valeur initiale et mettre à jour l'affichage.
+    function resetTimer() {
+        updateTimer(timeLeft);
+    }
+
+    // Écouter l'événement 'ended' sur l'élément audio pour réinitialiser le timer.
     endSound.addEventListener('ended', resetTimer);
 
-    // Démarrer le timer.
-    startTimer();
+    // Ajouter l'écoute des clics sur les boutons de temps prédéfinis.
+    timeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            updateTimer(this.dataset.time);
+        });
+    });
+
+    // Démarrer le timer avec le temps choisi.
+    startButton.addEventListener('click', function() {
+        var customTime = parseInt(customTimeInput.value, 10);
+        if (!isNaN(customTime) && customTime > 0) {
+            updateTimer(customTime);
+        }
+        startTimer();
+    });
 });
