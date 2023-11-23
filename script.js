68b5a64cd9088191583e007a412e3ce220e9e19f
tricepad <tricepa.dornon@gmail.com>
2023-11-23 16:48:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 document.getElementById('startButton').addEventListener('click', function() {
-    console.log('Bouton démarrer cliqué'); // Pour vérifier que l'événement de clic est capturé
-    var timeLeft = 30;
+    var initialTime = 30; // Valeur initiale du timer
+    var timeLeft = initialTime;
     var timerElement = document.getElementById('timer');
-    if (!timerElement) {
-        console.log('Élément timer non trouvé'); // Vérifie si l'élément timer est trouvé
-    }
     var interval = setInterval(function() {
-        console.log('Timer: ' + timeLeft); // Affiche la valeur actuelle du timer
         if (timeLeft <= 0) {
             clearInterval(interval);
-            var endSound = document.getElementById('endSound');
-            if (endSound) {
-                endSound.play();
-            } else {
-                console.log('Élément sonore non trouvé'); // Vérifie si l'élément sonore est trouvé
-            }
+            document.getElementById('endSound').play();
+            timeLeft = initialTime; // Réinitialiser le temps
+            timerElement.textContent = initialTime; // Mettre à jour l'affichage
+            // Optionnel : Redémarrer automatiquement ou permettre à l'utilisateur de redémarrer manuellement
+        } else {
+            timerElement.textContent = timeLeft;
+            timeLeft -= 1;
         }
-        timerElement.textContent = timeLeft;
-        timeLeft -= 1;
     }, 1000);
 });
