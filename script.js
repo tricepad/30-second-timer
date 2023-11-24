a41328a57f9abd11dccb4ea80c4fb27d9b684604
tricepad <tricepa.dornon@gmail.com>
2023-11-24 08:55:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,46 +3,37 @@ document.addEventListener('DOMContentLoaded', function() {
     var endSound = document.getElementById('endSound');
     var startButton = document.getElementById('startButton');
     var stopButton = document.getElementById('stopButton');
-    var timeButtons = document.querySelectorAll('.timeButton');
+    var timeLeft = 30;
     var interval;
-    var lastTimeSelected = 30; // Conserver la dernière valeur sélectionnée
-    var timeLeft = lastTimeSelected; // Initialiser timeLeft avec lastTimeSelected
 
     function updateTimerDisplay(newTime) {
-        lastTimeSelected = newTime; // Mettre à jour lastTimeSelected avec le nouveau temps
-        timeLeft = newTime; // Mettre à jour timeLeft avec le nouveau temps
-        timerElement.textContent = timeLeft; // Afficher le nouveau temps
+        timerElement.textContent = newTime;
     }
 
     function startTimer() {
         clearInterval(interval);
+        updateTimerDisplay(timeLeft);
         interval = setInterval(function() {
+            timeLeft -= 1;
+            updateTimerDisplay(timeLeft);
             if (timeLeft <= 0) {
                 clearInterval(interval);
                 endSound.play();
-            } else {
-                timerElement.textContent = timeLeft;
-                timeLeft--;
             }
         }, 1000);
     }
 
     function stopTimer() {
         clearInterval(interval);
-        updateTimerDisplay(lastTimeSelected); // Réinitialiser le timer à lastTimeSelected
+        timeLeft = 30;
+        updateTimerDisplay(timeLeft);
     }
 
-    startButton.addEventListener('click', function() {
-    var customTime = parseInt(customTimeInput.value, 10);
-    if (!isNaN(customTime) && customTime > 0) {
-        updateTimerDisplay(customTime);
-        startTimer(); // Démarrez le timer avec la valeur personnalisée
-    }
-});
-
+    startButton.addEventListener('click', startTimer);
     stopButton.addEventListener('click', stopTimer);
 
     endSound.addEventListener('ended', function() {
-        updateTimerDisplay(30); // Réinitialiser le timer après la fin du son
+        timeLeft = 30;
+        updateTimerDisplay(timeLeft);
     });
 });
